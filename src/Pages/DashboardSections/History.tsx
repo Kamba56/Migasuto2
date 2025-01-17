69031a7fe0b15822ab291503b1cc44a73a1a09e1
hamidusman <abdulhamidusman218@gmail.com>
2025-01-17 14:22:49 +0100

    reduced margin

--- src/Pages/DashboardSections/History.tsx
+++ src/Pages/DashboardSections/History.tsx
@@ -3,8 +3,10 @@ import CategoryLayout from "../../Components/CategoryLayout";
 export default function History(){
     return(
         <CategoryLayout
-            organization="Migasuto Team">
-            Hello world
+            organization="Migasuto Team"
+            category="Questionnaire History">
+            
+            <p>Hello world</p>
 
         </CategoryLayout>
     )
