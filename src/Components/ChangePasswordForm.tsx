95b5aac1c0ca6e21f76398da423396790d4cdc44
Hamza1610 <muhammadhamza162003@gmail.com>
2025-01-25 17:23:49 +0100

    Add ChangePasswordForm component for password management

---
+++ src/Components/ChangePasswordForm.tsx
@@ -0,0 +1,12 @@
+
+
+const ChangePasswordForm: React.FC = () => {
+    // Change password form component
+    return (
+      <div className="flex justify-between items-start">
+        {/* Change password form content */}
+      </div>
+    );
+};
+
+export default ChangePasswordForm;
