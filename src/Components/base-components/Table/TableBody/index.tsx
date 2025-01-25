94652e0ad5824eec5b43b7946cbd7ca2536f031c
sheriff adeiza <sheriffadeiza10@gmail.com>
2025-01-25 10:53:25 -0800

    sharp

--- src/Components/base-components/Table/TableBody/index.tsx
+++
@@ -1,8 +0,0 @@
-interface TableBodyProp {
-    body: string;
-}
-export default function TableBody({body}: TableBodyProp) {
-    return (
-        <td className="sm:px-4">{body}</td>
-    )
-}
--- src/Components/base-components/Table/TableHead/index.tsx
+++
@@ -1,9 +0,0 @@
-interface TableHeaderProp {
-    header: string;
-}
-
-export default function TableHeader({ header }: TableHeaderProp) {
-    return (
-        <th className="font-semibold px-4 py-2">{header}</th>
-    );
-}
