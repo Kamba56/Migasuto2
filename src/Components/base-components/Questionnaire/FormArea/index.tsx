c5c205b6e0ebdf60b12db525397f41ea24e9daec
hamidusman <abdulhamidusman218@gmail.com>
2025-01-30 06:07:24 +0100

    client questionnaire page

--- src/Components/base-components/Questionnaire/FormArea/index.tsx
+++ src/Components/base-components/Questionnaire/FormArea/index.tsx
@@ -5,12 +5,12 @@ interface FormTextProps {
 
 export default function FormArea({ title, placeholder }: FormTextProps) {
     return (
-        <div className="w-[280px] sm:w-[348px] ">
+        <div className="w-[280px] sm:w-[348px] flex flex-col gap-1 ">
             <label
                 className="text-black text-[16px]"
                 htmlFor="">{title} <span className="fottext-red-500">*</span></label>
             <div className=" p-4 border border-gray rounded-lg">
-                <textarea className="w-full p-0 h-[169px] border-0 focus:ring-0"
+                <textarea className="resize-none w-full p-0 h-[169px] border-0 focus:ring-0"
                     placeholder={`${placeholder}`}
                     />
             </div>
--- src/Components/base-components/Questionnaire/FormInput/multiple.tsx
+++ src/Components/base-components/Questionnaire/FormInput/multiple.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import FormInputProps from "../types";
+import cross from "../../../../assets/icons/cross.svg";
 
-export default function MultipleInput({ title, placeholder, type }: FormInputProps) {
+export default function MultipleInput(){
     const [values, setValues] = useState<string[]>([]);
 
 
@@ -22,37 +22,36 @@ export default function MultipleInput({ title, placeholder, type }: FormInputPro
 
     return (
         <div>
-        <label className="text-[16px]">
-            {title} <span className="text-red-500">*</span>
-        </label>
+            <label className="text-[16px]">
+                What are your core values <span className="text-red-500">*</span>
+            </label>
 
-        {/* Input */}
-        <div className="w-[280px] sm:w-[348px] p-4 border border-gray rounded-lg">
-            <input
-            type={type}
-            placeholder={placeholder}
-            onKeyDown={handleAddValue}
-            className="w-full p-0 text-[13px] h-[21px] border-0 focus:ring-0"
-            />
-        </div>
-
-        {/* Display Added Values */}
-        <div className="flex flex-wrap gap-2 mt-2">
-            {values.map((value, index) => (
-            <div
-                key={index}
-                className="bg-blue_fade text-primary w-fit p-3 flex gap-4 items-center rounded-md"
-            >
-                <span>{value}</span>
-                <button
-                onClick={() => removeValue(index)}
-                className="text-black font-bold"
-                >
-                X
-                </button>
+            <div className="w-[280px] sm:w-[348px] p-4 border border-gray rounded-lg">
+                <input
+                type="text"
+                placeholder="Enter your core values, press enter to add"
+                onKeyDown={handleAddValue}
+                className="w-full p-0 text-[13px] h-[21px] border-0 focus:ring-0"
+                />
             </div>
-            ))}
-        </div>
+
+            {/* Display Added Values */}
+            <div className="flex flex-wrap gap-2 mt-2">
+                {values.map((value, index) => (
+                <div
+                    key={index}
+                    className="bg-blue_fade text-primary w-fit p-3 flex gap-4 items-center rounded-md"
+                >
+                    <span>{value}</span>
+                    <button
+                    onClick={() => removeValue(index)}
+                    className="text-black font-bold"
+                    >
+                        <img src={cross} alt="cross" />
+                    </button>
+                </div>
+                ))}
+            </div>
         </div>
     );
 }
