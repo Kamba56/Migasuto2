8c7f5cc4a909db0305234e6bbe1ef7c021d82b34
Toma John Musa <tomajohn316@yahoo.com>
2025-02-15 14:34:12 +0100

    Add meetings page

--- src/Components/Meetingnote.tsx
+++ src/Components/Meetingnote.tsx
@@ -1,19 +1,13 @@
-import React from "react";
-import { BiCategory } from "react-icons/bi";
 import { BiPlus } from "react-icons/bi";
 
 const Meetingnote = () => {
   return (
     <div className="w-full">
-      <main className="flex-1  p-6 sm:p-6">
-        <header className="flex flex-col sm:flex-row justify-between  items-center mb-6">
-          <div className="flex mb-[20px] sm:mb-0 gap-3 items-center">
-            <BiCategory className="mt-5px" size={20} />
-            <h1 className="text-2xl  text-[24px] font-[600]">Meeting Notes</h1>
-          </div>
+      <main className="flex-1">
+        <header className="flex flex-col sm:flex-row justify-end  items-center mb-6">
           <button
             style={{ backgroundColor: "#336DBA" }}
-            className="w-[269px]  max-h-[730px] h-[49px] py-2 px-4 flex  items-center bg-blue-600 text-white rounded-md hover:bg-blue-700"
+            className="w-[269px] max-h-[730px] h-[49px] py-2 px-4 flex  items-center bg-blue-600 text-white rounded-md hover:bg-blue-700"
           >
             <BiPlus className="mr-2  border-2 rounded" size={20} />
             <span className="text-[#FEFEFE] ml-0 text-[14px]  sm:text-base font-medium">
