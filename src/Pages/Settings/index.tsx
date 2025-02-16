26e735c8d9e2c8b88d2a902bc9b835bd276a57b9
Taterbro <veecipher@gmail.com>
2025-02-16 21:03:10 +0100

    finished design
    finished the design for large screens

--- src/Pages/Settings/index.tsx
+++ src/Pages/Settings/index.tsx
@@ -1,3 +1,60 @@
+import FormInput from "../../Components/base-components/Form/FormInput/index";
+import { FormLabel } from "../../Components/base-components/Form";
+import { useState } from "react";
 export default function Settings2() {
-  return <div className="min-h-full rounded-[30px]"></div>;
+  const [isPasswordActive, setIsPasswordActive] = useState(true);
+  const [isProfileActive, setIsProfileActive] = useState(false);
+  return (
+    <div className="min-h-[40rem] rounded-[30px] bg-gray text-[16px]  pt-[4.5em] px-[5em]">
+      <div className="flex justify-around items-center">
+        <div>
+          <h1 className="text-[2em] font-[600] mb-[0.22em]">Change Password</h1>
+          <p className="text-[0.8em] font-[400]">
+            Add a password to make your account more secure
+          </p>
+        </div>
+        <div className="flex bg-[#ECF0FF] min-w-[22em] min-h-[3em] text-[1em] justify-around rounded-[0.5em]">
+          <button
+            className={`${
+              isProfileActive ? "bg-[#336DBA] text-white " : "text-[#9C9AA5] "
+            } rounded-[0.5em] min-w-[50%]`}
+            onClick={() => {
+              setIsProfileActive(true);
+              setIsPasswordActive(false);
+            }}
+          >
+            Profile
+          </button>
+          <button
+            className={`${
+              isPasswordActive ? "bg-[#465FF1] text-white " : "text-[#9C9AA5] "
+            } rounded-[0.5em] min-w-[50%]`}
+            onClick={() => {
+              setIsProfileActive(false);
+              setIsPasswordActive(true);
+            }}
+          >
+            Password
+          </button>
+        </div>
+      </div>
+      <div className="mt-[4.62em]  flex justify-center">
+        <form action="" className="max-w-[23em]">
+          <FormLabel className="font-[500]">Enter Password</FormLabel>
+          <FormInput formInputSize="lg" placeholder="XXXX XXXX X4380" />
+
+          <FormLabel className="mt-[2em] font-[500]">Verify Password</FormLabel>
+          <FormInput
+            className=" mb-[2.6em] "
+            formInputSize="lg"
+            placeholder="XXXX XXXX X4380"
+          />
+
+          <button className="w-[23.75em] h-[2.8em] bg-[#336DBA] rounded-[0.5em] text-white">
+            Change Password
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
