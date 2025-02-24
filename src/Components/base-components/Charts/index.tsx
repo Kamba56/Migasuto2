b947d90496f083c9c468fcde452d1288d81ceb51
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-02-24 19:39:18 +0100

    chart design update

--- src/Components/base-components/Charts/index.tsx
+++ src/Components/base-components/Charts/index.tsx
@@ -1,3 +1,4 @@
+import { IoMdInformationCircle } from "react-icons/io";
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 const pieData1 = [
@@ -18,28 +19,48 @@ export default function MetricChart() {
     <div className="flex flex-wrap gap-6 justify-center p-6">
       {/* Pie Chart 1 */}
       <div className="shadow-lg p-4 rounded-lg bg-white">
-        <h3 className="text-lg font-semibold">Title</h3>
-        <p className="text-gray-500">Short Description</p>
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-lg font-semibold">Title</h3>
+            <p className="text-gray-500">Short Description</p>
+          </div>
+          <IoMdInformationCircle
+            color="#BDBDBD"
+            size={21}/>
+        </div>
         <PieChart width={250} height={250}>
           <Pie
             data={pieData1}
             cx="50%"
             cy="50%"
             dataKey="value"
+            stroke="none"
           >
             {pieData1.map((entry, index) => (
               <Cell className="flex" key={`cell-${index}`} fill={entry.color} />
             ))}
           </Pie>
-          <Tooltip />
-          <Legend />
+          <Tooltip contentStyle={{ fontSize: "10px" }} />
+          <Legend 
+            wrapperStyle={{ fontSize: "10px" }} 
+            iconType="circle" 
+            layout="horizontal" 
+            align="left" 
+          />
         </PieChart>
       </div>
 
       {/* Pie Chart 2 */}
       <div className="shadow-lg p-4 rounded-lg bg-white">
-        <h3 className="text-lg font-semibold">Title</h3>
-        <p className="text-gray-500">Short Description</p>
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-lg font-semibold">Title</h3>
+            <p className="text-gray-500">Short Description</p>
+          </div>
+          <IoMdInformationCircle
+            color="#BDBDBD"
+            size={21}/>
+        </div>
         <PieChart width={250} height={250}>
           <Pie
             data={pieData2}
@@ -47,20 +68,33 @@ export default function MetricChart() {
             cy="50%"
             outerRadius={80}
             dataKey="value"
+            stroke="none"
           >
             {pieData2.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={entry.color} />
             ))}
           </Pie>
-          <Tooltip />
-          <Legend />
+          <Tooltip contentStyle={{ fontSize: "10px" }} />
+          <Legend 
+            wrapperStyle={{ fontSize: "10px" }} 
+            iconType="circle" 
+            layout="horizontal" 
+            align="left" 
+          />
         </PieChart>
       </div>
 
       {/* Doughnut Chart */}
       <div className="shadow-lg p-4 rounded-lg bg-white">
-        <h3 className="text-lg font-semibold">Title</h3>
-        <p className="text-gray-500">Short Description</p>
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-lg font-semibold">Title</h3>
+            <p className="text-gray-500">Short Description</p>
+          </div>
+          <IoMdInformationCircle
+            color="#BDBDBD"
+            size={21}/>
+        </div>
         <PieChart width={250} height={250}>
           <Pie
             data={pieData2}
@@ -69,13 +103,19 @@ export default function MetricChart() {
             innerRadius={50}
             outerRadius={80}
             dataKey="value"
+            stroke="none"
           >
             {pieData2.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={entry.color} />
             ))}
           </Pie>
-          <Tooltip />
-          <Legend />
+          <Tooltip contentStyle={{ fontSize: "10px" }} />
+          <Legend 
+            wrapperStyle={{ fontSize: "10px" }} 
+            iconType="circle" 
+            layout="horizontal" 
+            align="left" 
+          />
         </PieChart>
       </div>
     </div>
