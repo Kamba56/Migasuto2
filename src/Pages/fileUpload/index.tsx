0d7b7d6f68d3d0af2493b6bc0695320a5263b488
Taterbro <veecipher@gmail.com>
2025-02-26 08:43:10 +0100

    almost done

--- src/Pages/fileUpload/index.tsx
+++ src/Pages/fileUpload/index.tsx
@@ -3,9 +3,14 @@ import back from "../../assets/images/back.png";
 import bPlus from "../../assets/images/bluePlus.png";
 import wPlus from "../../assets/images/whitePlus.png";
 import { Link } from "react-router";
+import { useState } from "react";
 import Dropzone from "react-dropzone";
 
 export default function FileUpload() {
+  const [files, setFiles] = useState<File[]>([]);
+  const [progress, setProgress] = useState<
+    { filename: string; progress: number }[]
+  >([]); //just here for testing
   return (
     <div className="min-w-[67em] min-h-[46em] rounded-[1.87em] bg-white p-[1.75em]">
       <Link to={"/manager"} className="inline-block">
@@ -17,7 +22,14 @@ export default function FileUpload() {
 
       <div className="flex">
         <div>
-          <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+          <Dropzone
+            onDrop={(acceptedFiles) => {
+              setFiles((previtems) => [...previtems, ...acceptedFiles]);
+              setProgress((prev) => [...prev]);
+              console.log(acceptedFiles);
+              console.log(files);
+            }}
+          >
             {({ getRootProps, getInputProps }) => (
               <section>
                 <div
@@ -61,11 +73,20 @@ export default function FileUpload() {
               className="min-h-[10.5rem] mb-[1.43em] min-w-[23.75rem] border-[1px] rounded-[0.5em] text-[1em] placeholder:text-[1em]"
               placeholder="Enter your response"
             ></textarea>
-            <button className="min-w-[18.5em] text-left flex text-[0.87em] rounded-[0.5em] border-[1px] border-blue-20 text-blue-20 font-[500] bg-white p-[0.75em]">
+            <button className="min-w-[18.5em] text-left flex text-[0.87em] rounded-[0.5em] border-[1px] border-blue-20 text-blue-20 font-[500] bg-white p-[0.75em] mb-10">
               <img src={bPlus} alt="" className="w-[1.25em] h-auto mr-2" />
               Request A Meeting on File
             </button>
           </form>
+          {files.length > 0 && (
+            <div className="flex flex-col w-[27.8em]">
+              <p className="text-[0.87em] font-[500]">Uploading - 1/3 files</p>
+              <div className="w-full h-[2.3em] border-[#E3E3E3] border-[1px] rounded-[0.25em] text-[0.75em] flex flex-col justify-center">
+                <p className="ml-2">fileName</p>
+                <div className="bg-[#483EA8] h-[3px] w-[40%] rounded-[4px]"></div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
